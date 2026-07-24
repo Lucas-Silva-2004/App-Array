08eb70b6529cf7ed373725f6be66f9d03a89ac5e
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-24 20:00:04 -0300

    fiz formulario e botoes

---
+++ JavaScript/index.js
@@ -0,0 +1,19 @@
+let entradaTexto = document.getElementById("EntradaTexto");
+
+let btnAdicionar = document.getElementById("btnAdicionar");
+
+let btnOrdenar = document.getElementById("btnOrdenar");
+
+
+const objetos = ['Cadeira', 'Impressora', 'Garfo'];
+
+console.log(objetos);
+
+
+const verificarValor = () => {
+    document.getElementById("principal").className = 'Teste';
+    /* if (entradaTexto.value == null) {
+        console.log("Valor null");
+        alert("Informe um valor válido");
+    } */
+}
