68d60db71fc0fd4e5e1fe816004fa013acca0a82
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-24 20:52:38 -0300

    fiz validador de objetos que verifica se o objeto digitado já está presente no array

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,4 @@
-let entradaTexto = document.getElementById("EntradaTexto");
+const verificarValor = () => {
 
 let btnAdicionar = document.getElementById("btnAdicionar");
 
@@ -10,10 +10,27 @@ const objetos = ['Cadeira', 'Impressora', 'Garfo'];
 console.log(objetos);
 
 
-const verificarValor = () => {
-    document.getElementById("principal").className = 'Teste';
-    /* if (entradaTexto.value == null) {
+    const entradaTexto = document.getElementById("entradaTexto");
+    // Bloco para validar se a id está correta
+    /* if (!entradaTexto) {
+        console.log("O elemento com essa ID não foi encontrado no HTML");
+        return;
+    } */
+
+    if (entradaTexto.value.trim() === '') {
         console.log("Valor null");
         alert("Informe um valor válido");
-    } */
+    }
+    
+    else {
+        let indice = objetos.indexOf(entradaTexto.value);
+        if (indice > -1) {
+            alert("O objeto já foi adicionado");
+        }
+        else {
+            console.log("O objeto pode ser adicionado");
+            return;
+        }
+        console.log("erro desconhecido");
+    }
 }
