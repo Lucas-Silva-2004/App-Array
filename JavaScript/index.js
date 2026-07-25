499e6847c1426c938077f63865c36188bcdfb4e7
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-24 21:23:59 -0300

    funcao verificarValor() funcional

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,36 +1,32 @@
-const verificarValor = () => {
-
-let btnAdicionar = document.getElementById("btnAdicionar");
-
-let btnOrdenar = document.getElementById("btnOrdenar");
-
-
 const objetos = ['Cadeira', 'Impressora', 'Garfo'];
-
 console.log(objetos);
 
-
+const verificarValor = () => {
     const entradaTexto = document.getElementById("entradaTexto");
-    // Bloco para validar se a id está correta
+    // Bloco para validar se a id está correta, pois tive problema com nome da id: caractere errado.
     /* if (!entradaTexto) {
         console.log("O elemento com essa ID não foi encontrado no HTML");
         return;
     } */
 
+    // Se o campo estiver vazio, exiba um erro de valor inválido
     if (entradaTexto.value.trim() === '') {
         console.log("Valor null");
         alert("Informe um valor válido");
     }
-    
     else {
+        // Verifica se o valor do input existe no array
         let indice = objetos.indexOf(entradaTexto.value);
+
         if (indice > -1) {
-            alert("O objeto já foi adicionado");
+            alert("O objeto já existe");
         }
         else {
-            console.log("O objeto pode ser adicionado");
-            return;
+            // Adiciona o objeto escrito ao array
+            objetos.push(entradaTexto.value);
+            alert("Objeto adicionado ao array!");
+            console.log(objetos);
+            entradaTexto.value = '';
         }
-        console.log("erro desconhecido");
     }
 }
